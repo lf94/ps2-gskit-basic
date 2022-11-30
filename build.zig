204a1fe49edc5fa2f2adc240effe22f75f7c568e
daurnimator <quae@daurnimator.com>
2022-11-30 14:05:53 +1100

    WIP

--- build.zig
+++ build.zig
@@ -1,25 +1,34 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+    const target = std.zig.CrossTarget{
+        .cpu_arch = .mipsel,
+        .os_tag = .freestanding,
+        // need to get the target right for PS2.... I'm not sure how to tell zig about the 5900
+    };
 
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
+
+    // Fake libraries to workaround zig's `libcFullLinkFlags`
+    inline for (.{"dl", "rt", "util"}) |x| {
+        const lib = b.addStaticLibrary(x, "src/empty.c");
+        lib.setTarget(target);
+        lib.setBuildMode(mode);
+        lib.install();
+    }
 
     const exe = b.addExecutable("ps2.elf", "src/main.c");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.setLibCFile(std.build.FileSource.relative("./out.txt"));
     exe.defineCMacro("_EE", null);
-    exe.addIncludePath("/usr/local/ps2dev/ee/mips64r5900el-ps2-elf/include");
+    // exe.addIncludePath("/usr/local/ps2dev/ee/mips64r5900el-ps2-elf/include");
     exe.addIncludePath("/usr/local/ps2dev/ps2sdk/common/include");
     exe.addIncludePath("/usr/local/ps2dev/gsKit/include");
     exe.addLibraryPath("/usr/local/ps2dev/gsKit/lib");
+    exe.addLibraryPath("./zig-out/lib");
     exe.linkLibC();
     exe.install();
 
